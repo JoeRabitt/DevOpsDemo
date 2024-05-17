8b39f87a5307fbb2e1a9a89955387ec91c49560d
Joe <JoeRabitt@AOL.com>
2024-05-17 17:56:10 +0200

    update

--- backend/src/test/java/ch/zhaw/iwi/devops/demo/DoneTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/demo/DoneTest.java
@@ -13,4 +13,16 @@ class DoneTest {
         assertEquals("description", done1.getDescription());
         assertEquals(1, done1.getId());
     }
+
+    @Test
+    void testSetters() {
+        var done2 = new Done();
+        done2.setId(2);
+        done2.setTitle("neuer Titel");
+        done2.setDescription("neue Beschreibung");
+
+        assertEquals(2, done2.getId(), "ID sollte aktualisiert werden");
+        assertEquals("neuer Titel", done2.getTitle(), "Titel sollte aktualisiert werden");
+        assertEquals("neue Beschreibung", done2.getDescription(), "Beschreibung sollte aktualisiert werden");
+    }    
 }
